fa60f8144d186787bf25f67cb7161375a9fe3feb
paul.lungu <paul.lungu@camunda.com>
2023-01-05 19:19:36 -0700

    test-sso

---
+++ extension-sso-keycloak/src/main/java/org/camunda/bpm/extension/keycloak/App.java
@@ -0,0 +1,10 @@
+package org.camunda.bpm.extension.keycloak;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class App {
+}
